de4844a6ee68f05a9db6a4a8f9176d8885593ea2
aborsim <aborsim@users.noreply.github.com>
2015-03-01 00:31:35 -0500

    Finished socket library.

--- socketry.cpp
+++ socketry.cpp
@@ -1,4 +1,4 @@
-#include "socketry.h"
+ #include "socketry.h"
 
 using namespace socketry;
 
@@ -6,11 +6,8 @@ Socketry::Socketry(char netType)
 {
     type = netType;
     server = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-    if(server < 0)
-    {
-        cerr << "Could not connect. Exiting.";
-        return;
-    }
+    if(server < 0) error("Could not connect.");
+    servAddr.sin_family = AF_INET;
     //Happy ending.
 }
 
@@ -19,30 +16,36 @@ Socketry::~Socketry()
     //Where I delete stuff. Todo.
 }
 
-int Socketry::connect(char* ip, int port)
+int Socketry::link(char* ip, int port)
 {
     if (type != CLIENT) //Check to make sure that the right initialization method is being used
     {
         std::cerr << "Wrong instantiation type. Omit the IP for a socket server.\n";
         return -1;
     }
-    servIP = gethostbyname(ip);
-    if (server == NULL) return -1;
-    
+
+    servIP = gethostbyname(ip); //Get IP from char*, and setup servAddr object
+    if (servIP == NULL) return -1;
+    memset((char *) &servAddr, 0, sizeof(servAddr)); //Make sure the servAddr object is empty
+    memcpy((char *)servIP->h_addr, (char *)&servAddr.sin_addr.s_addr, servIP->h_length); //Copy stuff from servIP to servAddr
+    servAddr.sin_port = htons(port);
+
+    if (connect(server, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) return -1; //Connect
+    return 0; //Happy ending.
 }
 
-int Socketry::start(int port, int queue = 5, bool broadcast = false)
+int Socketry::serve(int port, int queue = 5, bool broadcast = false)
 {
     if (type != SERVER) //Check to make sure that the right initialization method is being used
     {
         std::cerr << "Wrong instantiation type. Include the IP for a socket client.\n";
         return -1;
     }
-    sockAddr.sin_family = AF_INET; //Basic socket setup
-    sockAddr.sin_addr.s_addr = INADDR_ANY;
-    if (broadcast) sockAddr.sin_addr.s_addr = INADDR_BROADCAST;
-    sockAddr.sin_port = htons(port);
-    if (bind(server, (const sockaddr *) &sockAddr, sizeof(sockAddr)) < 0) return -1; //Error checking
+
+    servAddr.sin_addr.s_addr = INADDR_ANY; //Basic socket setup
+    if (broadcast) servAddr.sin_addr.s_addr = INADDR_BROADCAST;
+    servAddr.sin_port = htons(port);
+    if (bind(server, (const sockaddr *) &servAddr, sizeof(servAddr)) < 0) return -1; //Error checking
     listen(server, queue);
     clilen = sizeof(client);
     client = accept(server, (struct sockaddr *) &client, &clilen); //Receive connection into cliocket
@@ -50,3 +53,26 @@ int Socketry::start(int port, int queue = 5, bool broadcast = false)
     return 0; //Happy ending.
 }
 
+int Socketry::send(char* data)
+{
+    if (write(server, data, strlen(data)) < 0) return -1;
+    return 0; //Happy ending.
+}
+
+char* Socketry::receive()
+{
+    memset(buffer, 0, 10);
+    if (read(server, buffer, 10) < 0) return "\0";
+    return buffer; //Happy ending.
+}
+
+void Socketry::end()
+{
+    close(server); close(client); //Happy endings for everyone.
+}
+
+void error(char* msg)
+{
+    std::cerr << msg;
+    std::exit(-1);
+}
--- socketry.h
+++ socketry.h
@@ -5,11 +5,15 @@
 #ifndef TALKATIVE_H
 #define TALKATIVE_H
 
+#include <cstdlib>
+#include <unistd.h>
 #include <iostream>
 #include <sys/types.h>   // Types used in sys/socket.h and netinet/in.h
 #include <netinet/in.h>  // Internet domain address structures and functions
+#include <sys/types.h>
 #include <sys/socket.h>  // Structures and functions used for socket API
 #include <netdb.h>       // Used for domain/DNS hostname lookup
+#include <string.h>
 
 using namespace std;
 
@@ -24,20 +28,23 @@ class Socketry
 public:
     Socketry(char);
     ~Socketry();
-    int connect(char*, int);
-    int start(int, int, bool);
+    int link(char*, int);
+    int serve(int, int, bool);
     int send(char*);
     char* receive();
+    void end();
 
 private:
     int server, client;
     char type;
     socklen_t clilen;
-    char buffer[256];
-    struct sockaddr_in sockAddr, cliAddr;
+    char buffer[10];
+    struct sockaddr_in servAddr, cliAddr;
     struct hostent *servIP;
 };
 
 }
+
+void error(char*);
 
 #endif // TALKATIVE_H
