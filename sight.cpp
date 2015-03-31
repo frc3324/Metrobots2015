5d382b6e35251f4f00d1907014c94098091bd466
Insert Hometown Here <aborsim@users.noreply.github.com>
2015-03-31 19:16:05 -0400

    Final vision branch

--- sight.cpp
+++ sight.cpp
@@ -4,55 +4,57 @@
 using namespace cv;
 using namespace std;
 
-Sight::Sight(int camera, int width, int height)
+Sight::Sight(int camera, int width = 640, int height = 480)
 {
     cam = VideoCapture(camera);
     cam.set(3, width);
     cam.set(4, height);
-    focalWidth = (3.6 / 3.674) * width;
-    baseImg = imread("/home/myself/Code/Vision2015/baseimg.png", 0);
-
-    detector.detect(baseImg, basePoints);
-    extractor.compute(baseImg, basePoints, baseDescriptors);
-    if (baseDescriptors.empty()) exit(-1);
 }
 
 Sight::~Sight()
 {
-    /* Absolutely nothing. */
+
 }
 
-void Sight::preCompute()
+Mat Sight::getThresholded()
 {
-    cam.read(frame);
-    detector.detect(frame, camPoints);
-    extractor.compute(frame, camPoints, camDescriptors);
+    Mat frame; cam.read(frame);
+    Mat hsv; cvtColor(frame, hsv, COLOR_BGR2HSV);
+    Mat thresholded;
+
+    inRange(hsv, min, max, thresholded);
+
+    erode(thresholded, thresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+    dilate(thresholded, thresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+    dilate(thresholded, thresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+    erode(thresholded, thresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+    return thresholded;
 }
 
-void Sight::getTotes() {
-    extractor.compute(frame, camPoints, camDescriptors);
-    matcher.match(baseDescriptors, camDescriptors, matches);
-    drawMatches(baseImg, basePoints, frame, camPoints, matches, imgMatches);
-}
+vector<RotatedRect> Sight::getTotes(vector<Vec4i> &hierarchy) {
+    Mat thresholded = getThresholded();
+    vector< vector<Point> > contours;
+    findContours(thresholded, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-vector< pair<float, float> > Sight::getInfo(vector<Rect> rects, float widthmm = 683)
-{
-    vector< pair<float, float> > drects;
-    int widthpx;
-    float distance, lateral;
-    for (Rect rect: rects)
+    Rect rect;
+    vector<RotatedRect> totes;
+    for (int i = 0; i < contours.size(); i++)
     {
-        distance = (widthmm * focalWidth) / rect.width;
-
-        widthpx = (rect.x + .5 * rect.width) - cam.get(3) / 2;
-        lateral = widthpx * (rect.width / widthmm);
-
-        drects.push_back(make_pair(distance, lateral));
+        if (contourArea(contours.at(i)) > 200)
+            totes.push_back(minAreaRect(contours.at(i)));
     }
-    return drects;
+    return totes;
 }
 
-Mat Sight::update() {
-    preCompute();
-    getTotes();
+Mat Sight::getFrame() {
+    Mat frame; cam.read(frame);
+    vector<Vec4i> hierarchy;
+    vector<RotatedRect> totes = getTotes(hierarchy);
+    Scalar color = Scalar(255, 0, 0);
+    for(int i = 0; i < totes.size(); i++)
+    {
+         ellipse(frame, totes.at(i), color, 2);
+    }
+    return frame;
 }
